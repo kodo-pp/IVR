cf4bb15eff4f8fade7e6d2a131fe736c40f60c44
Alexander Korzun <korzun.sas@mail.ru>
2018-01-04 19:23:15 +0300

    Код инициализации Irrlicht вынесен в отдельную функцию в src/graphics/graphics.cpp

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -5,6 +5,11 @@
 
 #include <unistd.h>
 
+/**
+ * Глобальные переменные, хранящие необходимые объекты для работы с Irrlicht
+ *
+ * Отдельное пространство имён для изоляции
+ */
 namespace graphics {
     irr::IrrlichtDevice * irrDevice = nullptr;
     irr::video::IVideoDriver * irrVideoDriver = nullptr;
@@ -12,12 +17,42 @@ namespace graphics {
     irr::gui::IGUIEnvironment * irrGuiEnvironment = nullptr;
 }
 
+static bool initializeIrrlicht(std::vector <std::string> * args);
+
 bool initializeGraphics(std::vector <std::string> * args) {
     if (args == nullptr) {
         return false;
     }
 
+    if (!initializeIrrlicht(args)) {
+        return false;
+    }
+
     // TODO: возможно, вынести код инициализации Irrlicht в отдельныю функцию
+
+
+    // ***********************************************************************
+    // Тест, потом убрать
+
+    graphics::irrGuiEnvironment->addStaticText(L"Hello world!", // Сам текст
+        irr::core::rect <irr::s32> (10, 10, 260, 260), // Положение текста на экране (прямоугольная область)
+        true); // Неясно, что это
+
+    graphics::irrVideoDriver->beginScene(true, // Неясно, что это
+        true, // Неясно, что это
+        irr::video::SColor(255, 100, 101, 140)); // Какой-то цвет, возможно, цвет фона (ARGB)
+    graphics::irrGuiEnvironment->drawAll();
+    graphics::irrVideoDriver->endScene();
+    sleep(3);
+    graphics::irrDevice->drop();
+
+    // Конец теста
+    // ***********************************************************************
+    return true;
+}
+
+// Инициаллизация Irrlicht
+static bool initializeIrrlicht(std::vector < std::string > * args) {
     graphics::irrDevice = irr::createDevice(
         irr::video::EDT_OPENGL, // Драйвер для рендеринга (здесь OpenGL) (см. http://irrlicht.sourceforge.net/docu/example001.html)
         irr::core::dimension2d <irr::u32> (800, 600), // Размеры окна (не в полноэкранном режиме)
@@ -48,23 +83,5 @@ bool initializeGraphics(std::vector <std::string> * args) {
     if (!graphics::irrSceneManager) {
         return false;
     }
-
-    // ***********************************************************************
-    // Тест, потом убрать
-
-    graphics::irrGuiEnvironment->addStaticText(L"Hello world!", // Сам текст
-        irr::core::rect <irr::s32> (10, 10, 260, 260), // Положение текста на экране (прямоугольная область)
-        true); // Неясно, что это
-
-    graphics::irrVideoDriver->beginScene(true, // Неясно, что это
-        true, // Неясно, что это
-        irr::video::SColor(255, 100, 101, 140)); // Какой-то цвет, возможно, цвет фона (ARGB)
-    graphics::irrGuiEnvironment->drawAll();
-    graphics::irrVideoDriver->endScene();
-    sleep(3);
-    graphics::irrDevice->drop();
-
-    // Конец теста
-    // ***********************************************************************
     return true;
 }
