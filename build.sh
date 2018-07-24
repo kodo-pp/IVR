e2d45b8d1d7cb620881554a2dba0aba8a9e38d68
Alexander Korzun <korzun.sas@mail.ru>
2018-07-24 17:41:44 +0300

    [WIP] Begin migrating to clang

--- build.sh
+++ build.sh
@@ -1,15 +1,66 @@
 #!/usr/bin/env bash
 
+# This script builds ModBox. For more information see ./build.sh --help
+
+if [[ "$1" == "--help" ]]; then
+    echo -e "Usage: $0 [--help]"
+    echo -e "Build ModBox from its source, assuming that all dependencies are already installed."
+    echo -e ""
+    echo -e "You can pass the following environmental variables to this script:"
+    echo -e "  DEBUG=[yes/no]               - build a debug (= yes) or release (= no) version of ModBox"
+    echo -e "  FORCE_REBUILD=[yes/no]       - force rebuilding all sources (= yes) or only changed ones (= no)"
+    echo -e "  CC_TOOLCHAIN=[gcc/clang/custom/generic] - toolchain used to build ModBox"
+    echo -e "    (if CC_TOOLCHAIN=custom)"
+    echo -e "      CUSTOM_CC                - C compiler"
+    echo -e "      CUSTOM_CXX               - C++ compiler"
+    echo -e "      CUSTOM_LD                - Linker, usually the same as CUSTOM_CXX"
+    echo -e ""
+    echo -e "Default settings are:"
+    echo -e "  DEBUG=no"
+    echo -e "  FORCE_REBUILD=no"
+    echo -e "  CC_TOOLCHAIN=generic"
+    echo -e ""
+    echo -e "Exact commands executed are written to build.log"
+    exit 1
+fi
+
 # Executable file name
 exec_name="main"
 
 # Level of optimization, ignored when environment variable 'DEBUG' is set to 'yes'
 optimization_level=2
 
-# Compilers
+# Optimization flags (used only when DEBUG=no)
+OPT_FLAGS="-O${optimization_level} -flto -ftree-vectorize"
+
+# Compilers - default values
 CC="cc"     # C compiler
 CXX="c++"   # C++ compiler
 LD="c++"    # Linker
+
+# We can change toolchain if user wants to
+shopt -s nocasematch
+if [[ "${CC_TOOLCHAIN}" == "gcc" ]]; then  # Force usage of GCC
+    CC="gcc"
+    CXX="g++"
+    LD="g++"
+elif [[ "${CC_TOOLCHAIN}" == "clang" ]]; then  # Force usage of Clang
+    CC="clang"
+    CXX="clang++"
+    LD="clang++"
+elif [[ "${CC_TOOLCHAIN}" == "generic" ]]; then  # Force usage of generic toolchain (/usr/bin/{cc,c++})
+    CC="cc"
+    CXX="c++"
+    LD="c++"
+elif [[ "${CC_TOOLCHAIN}" == "custom" ]]; then  # Custom toolchain
+    CC="${CUSTOM_CC}"
+    CXX="${CUSTOM_CXX}"
+    LD="${CUSTOM_LD}"
+elif [[ "${CC_TOOLCHAIN}" != "" ]]; then  # Specified but unknown toolchain
+    echo "Error: unknown toolchain: '${CC_TOOLCHAIN}'" >&2
+    exit 1
+fi
+shopt -u nocasematch
 
 # Compiler flags
 
@@ -36,14 +87,14 @@ INCLUDE_PATH="-Iinclude -I/usr/include/irrlicht"
 FLAGS="${FLAGS} ${INCLUDE_PATH}"
 
 # Dealing with debug mode
-if [ ".${DEBUG}" == '.yes' ]; then
+if [[ "${DEBUG}" == "yes" ]]; then
     # If we are debugging, set -g flag and disable optimizations
     FLAGS="${FLAGS} -g -O0 -DDEBUG_MODE"
     LDFLAGS="${LDFLAGS} -O0"
 else
-    # Othewise, optimizations are set to ${optimization_level}
-    LDFLAGS="${LDFLAGS} -O${optimization_level}"
-    FLAGS="${FLAGS} -O${optimization_level}"
+    # Othewise, optimizations are enabled
+    LDFLAGS="${LDFLAGS} ${OPT_FLAGS}"
+    FLAGS="${FLAGS} ${OPT_FLAGS}"
 fi
 
 # Project version
@@ -67,43 +118,44 @@ function run_command() {
     e="$?"
     # And if it is not 0 (EXIT_SUCCESS), then stop build process
     if [ "$e" -ne 0 ]; then
-        echo -e "\e[1;31mError: exit status $e\e[0m"
+        echo -e "\e[1;31mError: exit status $e\e[0m" >&2
         kill $$
     fi
 }
 
-# Pretty-print command. # Run command. The first argument is action suzh as 'cc'
+# Pretty-print command. The first argument is action such as 'cc'
 # (compile C file), 'c++' (compile C++ file), or 'link'. The last function argument
 # is the last command argument, and it means that is is a source/output file,
 # depending on command
 function dump_command() {
     case $1 in
     cc)
-        echo -ne '\e[1;34m[ CC ]  \e[0m'
+        echo -ne '\e[1;34m[ CC ]  \e[0m' >&2
         ;;
     cxx)
-        echo -ne '\e[1;34m[ C++ ] \e[0m'
+        echo -ne '\e[1;34m[ C++ ] \e[0m' >&2
         ;;
     link)
-        echo -ne '\e[1;35m[LINK]  \e[0m'
+        echo -ne '\e[1;35m[LINK]  \e[0m' >&2
         ;;
     strip)
-        echo -ne '\e[1;36m[STRIP] \e[0m'
+        echo -ne '\e[1;36m[STRIP] \e[0m' >&2
         ;;
     esac
 
     case $1 in
     cc|cxx)
-        # The last function argument, see https://stackoverflow.com/questions/1853946/getting-the-last-argument-passed-to-a-shell-script
+        # The last function argument, see
+        # https://stackoverflow.com/questions/1853946/getting-the-last-argument-passed-to-a-shell-script
         local src_file="${!#}"
-        echo "$src_file"
+        echo "$src_file" >&2
         ;;
     link)
         local final_exec_file="${!#}"
-        echo "-> $final_exec_file"
+        echo "-> $final_exec_file" >&2
         ;;
     strip)
-        echo "$exec_name"
+        echo "$exec_name" >&2
         ;;
     esac
 
@@ -157,7 +209,7 @@ function build_file() {
 }
 
 # OK, begin the build process
-echo -e '\e[1mBuilding...\e[0m'
+echo -e '\e[1mBuilding...\e[0m' >&2
 truncate -s 0 build.log
 
 # The list of object files, filled in while building
@@ -173,6 +225,6 @@ done
 run_command link "${LD}" ${objects} ${LDFLAGS} -o "${exec_name}"
 
 # Strip the resulting file if we don't want to debug it
-if [ ".$DEBUG" != ".yes" ]; then
+if [[ "${DEBUG}" != "yes" ]]; then
     run_command strip strip --strip-all "${exec_name}"
 fi
